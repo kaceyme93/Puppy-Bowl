967c24493d0919407c8375afa918b4ed68a53b07
Kacey Ellis <kaceyme93@gmail.com>
2022-04-14 21:55:44 -0400

    first commit

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,13 +1,19 @@
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'COHORT-NAME';
+const cohortName = '2202-FTB-ET-WEB-PT';
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
 
 export const fetchAllPlayers = async () => {
-
+    try {
+        const response = await fetch(`${APIURL}/players`);
+        const result = await response.json();
+        if (result.error) throw result.error;
+        return result.data.players;
+      } catch (err) {
+        console.error('Uh oh, trouble fetching players!', err);
+      }
 };
-
 export const fetchSinglePlayer = async (playerId) => {
 
 };
